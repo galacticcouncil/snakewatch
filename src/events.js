e479450dd0466a6f56cd423bb545c24c9c4b51ba
Lumir Mrkva <mrq@mrq.cz>
2023-03-29 22:32:20 +0200

    initialization events have siblings as well

--- src/events.js
+++ src/events.js
@@ -76,7 +76,7 @@ export const loadEvents = async blockNumber => api().rpc.chain.getBlockHash(bloc
 
 export const processEvents = events => events.map(event => ({
   siblings: events
-    .filter(({phase}) => phase.isApplyExtrinsic
+    .filter(({phase}) => phase.isInitialization || phase.isApplyExtrinsic
       && event.phase.isApplyExtrinsic
       && phase.asApplyExtrinsic.eq(event.phase.asApplyExtrinsic))
     .map(({event}) => event),

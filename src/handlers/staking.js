a7bf809a09f760c2bb2604cffb6227e1de7c11c7
mrq <mrq@mrq.cz>
2023-12-09 03:15:16 +0100

    staking hotfix

--- src/handlers/staking.js
+++ src/handlers/staking.js
@@ -15,11 +15,11 @@ async function stakeHandler({event: {data: {who, stake}}}) {
 }
 
 async function unstakeHandler({event: {data: {who, unlockedStake, rewards}}}) {
-  broadcast(`${formatAccount(who, false, emojify(who))} unstaked **${formatAmount(hdx(unlockedStake))}** <:cheems:989553853785587723> and claimed **${formatAmount(hdx(rewards))}**`);
+  broadcast(`${formatAccount(who, false, emojify(who))} unstaked **${formatAmount(hdx(unlockedStake))}** <:cheems:989553853785587723>`);
 }
 
 async function rewardsClaimedHandler({event: {data: {who, paidRewards, slashedUnpaidRewards}}}) {
   const percentage = new Intl.NumberFormat('en-US').format((paidRewards / (slashedUnpaidRewards+paidRewards)) * 100);
-  const message = `${formatAccount(who, false, emojify(who))} claimed **${formatAmount(hdx(paidRewards))}** (${percentage}% of allocated reward)`;
+  const message = `${formatAccount(who, false, emojify(who))} claimed **${formatAmount(hdx(paidRewards))}**`;
   broadcast(message);
 }

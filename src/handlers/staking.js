9f9cf11bb49b6dc0af2ea978b52ab076dd08e434
mrq <mrq@mrq.cz>
2024-01-10 14:59:18 +0100

    staking report total claimed rewards

--- src/handlers/staking.js
+++ src/handlers/staking.js
@@ -14,14 +14,15 @@ async function stakeHandler({event: {data: {who, stake}}}) {
   broadcast(`${formatAccount(who, false, emojify(who))} staked **${formatAmount(hdx(stake))}**`);
 }
 
-async function unstakeHandler({event, siblings}) {
+async function unstakeHandler({event}) {
   const {data: {who, unlockedStake}} = event;
   broadcast(`${formatAccount(who, false, emojify(who))} unstaked **${formatAmount(hdx(unlockedStake))}** <:cheems:989553853785587723>`);
 }
 
-async function rewardsClaimedHandler({event: {data: {who, paidRewards, slashedUnpaidRewards}}}) {
-  if (Number(paidRewards) > 0) {
-    const message = `${formatAccount(who, false, emojify(who))} claimed **${formatAmount(hdx(paidRewards))}** and forfeited **${formatAmount(hdx(slashedUnpaidRewards))}**`;
+async function rewardsClaimedHandler({event: {data: {who, paidRewards, unlockedRewards, slashedUnpaidRewards}}}) {
+  const totalRewards = Number(paidRewards) + Number(unlockedRewards);
+  if (totalRewards > 0) {
+    const message = `${formatAccount(who, false, emojify(who))} claimed **${formatAmount(hdx(totalRewards))}** and forfeited **${formatAmount(hdx(slashedUnpaidRewards))}**`;
     broadcast(message);
   }
 }
